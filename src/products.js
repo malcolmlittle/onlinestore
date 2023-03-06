f92f9a2573d99816f5384a27257f89bbe7a377f8
Malcolm Little <malcolmvlittle@gmail.com>
2023-03-05 18:48:42 -0800

    updated prices

--- src/products.js
+++ src/products.js
@@ -17,43 +17,43 @@ export const PRODUCTS = [
     {
         id: 2,
         productName: "MacBook Pro 2022 (M1)",
-        price: 999.0,
+        price: 1999.0,
         productImage: product2
     },
     {
         id: 3,
         productName: "Cannon M50 Camera",
-        price: 999.0,
+        price: 699.0,
         productImage: product3
     },
     {
         id: 4,
         productName: "WLS Van Gogh Denim Jacket",
-        price: 999.0,
+        price: 228.0,
         productImage: product4
     },
     {
         id: 5,
         productName: "LED Light Strips",
-        price: 999.0,
+        price: 19.99,
         productImage: product5
     },
     {
         id: 6,
         productName: "SPECTRUM LS TEE",
-        price: 999.0,
+        price: 68.0,
         productImage: product6
     },
     {
         id: 7,
         productName: "AUTO SREVICE SHIRT by GOLF WANG",
-        price: 999.0,
+        price: 120.0,
         productImage: product7
     },
     {
         id: 8,
         productName: "DON'T TRIP UNSTRUCTURED HAT",
-        price: 999.0,
+        price: 40.0,
         productImage: product8
     },
 ]

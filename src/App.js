28d0f77d4444ca49ec747a626d82f44167e86505
Malcolm Little <malcolmvlittle@gmail.com>
2023-03-05 17:14:28 -0800

    render Shop and Cart components

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 //classic imports that will always be used when working with react-router-dom
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Navbar } from "./components/navbar";
+import { Shop } from "./pages/shop/shop";
+import { Cart } from "./pages/cart/cart";
 
 function App() {
   return (
@@ -9,8 +11,8 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route path='/' />
-          <Route path='/cart' />
+          <Route path='/' element={ <Shop /> } />
+          <Route path='/cart' element={ <Cart /> }/>
         </Routes>
       </Router>
     </div>

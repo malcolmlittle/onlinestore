8e3c0e9a2d7a41f5a7a5a7c4c4a6c0df0080e0ac
mlittleDot <mlittle@dottys.com>
2023-03-06 15:08:17 -0800

    created Cart component that accesses the cartItems object from our ShopContext and renders items in cart

--- src/pages/cart/cart.jsx
+++ src/pages/cart/cart.jsx
@@ -1,9 +1,28 @@
-import React from "react";
+import React, { useContext } from "react";
+import { PRODUCTS } from "../../products";
+import { ShopContext } from "../../context/shop-context";
+import { CartItem } from "./cart-item";
+import "./cart.css";
 
 export const Cart = () => {
+
+    const { cartItems } = useContext(ShopContext)
+
     return (
-        <div>
-            Cart
+        <div className="cart">
+            <div>
+                <h1>Your Cart Items</h1>
+            </div>
+            <div className="cartItems">
+                {PRODUCTS.map((product) => {
+                    // only display the items from the products array that are also in our cart
+                    // if the cartItems object with the key equal to the product's id has a value greater than zero
+                    // this means that this product is in the cart
+                    if (cartItems[product.id] !== 0) {
+                        return <CartItem data={product} />
+                    }
+                })}
+            </div>
         </div>
     );
 }

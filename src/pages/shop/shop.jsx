da31a0ea84b48828422a97c3c5afa4c8c162b54c
Malcolm Little <malcolmvlittle@gmail.com>
2023-03-05 18:51:19 -0800

    updated Shop component to map thru and render products

--- src/pages/shop/shop.jsx
+++ src/pages/shop/shop.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { PRODUCTS } from "../../products";
+import { Product } from "./product";
+// these imported styles will apply to this Shop component and child components like Product component in line 16
+import "./shop.css"
 
 export const Shop = () => {
     return (
-        <div>Shop</div>
+        <div className="shop">
+            <div className="shopTitle">
+                <h1>Malcolm's Online Shop</h1>
+            </div>
+            <div className="products">
+                {PRODUCTS.map((product) => (
+                    // create a prop called data that will basically be the data that's passed down
+                    <Product data={product}/>
+                ))}
+            </div>
+        </div>
     );
 }

e1c1415f9a1255c7081d971954b134ce9552517a
Malcolm Little <malcolmvlittle@gmail.com>
2023-03-06 19:15:04 -0800

    added conditional if totalAmount in cart is 0 then show that cart is empty

--- src/pages/cart/cart.jsx
+++ src/pages/cart/cart.jsx
@@ -28,11 +28,15 @@ export const Cart = () => {
                 })}
             </div>
 
-            <div className="checkout">
+            {totalAmount > 0 ? (
+                <div className="checkout">
                 <p> Subtotal: ${totalAmount}</p>
                 <button onClick={() => navigate("/")}> Continue Shopping </button>
                 <button> Checkout </button>
             </div>
+            ) : (
+                <h1>Your Cart is Empty</h1>
+            )}
         </div>
     );
 }
